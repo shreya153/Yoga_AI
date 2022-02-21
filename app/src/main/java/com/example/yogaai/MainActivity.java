a762924f76ee13d26e192aa91b392de099334c7b
DELL <shreyasiva2002@gmail.com>
2022-02-21 11:39:21 +0530

    App

---
+++ app/src/main/java/com/example/yogaai/MainActivity.java
@@ -0,0 +1,36 @@
+package com.example.yogaai;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageButton;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        ImageButton exe = (ImageButton)findViewById(R.id.exerc);
+        ImageButton yoga = (ImageButton)findViewById(R.id.yoga);
+        /*exe.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(MainActivity.this,.class);
+                startActivity(intent);
+            }
+        });
+        yoga.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent i = new Intent(MainActivity.this, .class);
+                startActivity(i);
+            }
+        });*/
+
+
+    }
+}
