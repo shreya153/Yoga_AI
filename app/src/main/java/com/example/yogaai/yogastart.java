acdb112c785a644df7c14b0564554f59b3a17b36
DELL <shreyasiva2002@gmail.com>
2022-02-21 12:35:49 +0530

    App

--- app/src/main/java/com/example/yogaai/yogastart.java
+++ app/src/main/java/com/example/yogaai/yogastart.java
@@ -2,7 +2,10 @@ package com.example.yogaai;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageButton;
 
 public class yogastart extends AppCompatActivity {
 
@@ -10,5 +13,13 @@ public class yogastart extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_yogastart);
+        ImageButton back = findViewById(R.id.back);
+        back.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(yogastart.this,MainActivity.class);
+                startActivity(intent);
+            }
+        });
     }
 }
